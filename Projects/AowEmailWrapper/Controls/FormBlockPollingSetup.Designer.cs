288fad001631f83fd00cb0f2a19247b3cbf65604
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-08-06 19:43:15 +0000

    New control for Incoming Email settings

---
+++ Projects/AowEmailWrapper/Controls/FormBlockPollingSetup.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace AowEmailWrapper.Controls
+{
+    partial class FormBlockPollingSetup
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.panelLabel = new System.Windows.Forms.Panel();
+            this.labelEvery = new System.Windows.Forms.Label();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.panelLabel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.Dock = System.Windows.Forms.DockStyle.Right;
+            this.comboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(191, 0);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(216, 21);
+            this.comboBox.TabIndex = 9;
+            // 
+            // panelLabel
+            // 
+            this.panelLabel.AutoSize = true;
+            this.panelLabel.Controls.Add(this.labelEvery);
+            this.panelLabel.Controls.Add(this.checkBox);
+            this.panelLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelLabel.Location = new System.Drawing.Point(0, 0);
+            this.panelLabel.Margin = new System.Windows.Forms.Padding(2);
+            this.panelLabel.Name = "panelLabel";
+            this.panelLabel.Size = new System.Drawing.Size(191, 29);
+            this.panelLabel.TabIndex = 10;
+            // 
+            // labelEvery
+            // 
+            this.labelEvery.BackColor = System.Drawing.SystemColors.Control;
+            this.labelEvery.Dock = System.Windows.Forms.DockStyle.Right;
+            this.labelEvery.Location = new System.Drawing.Point(141, 0);
+            this.labelEvery.Name = "labelEvery";
+            this.labelEvery.Size = new System.Drawing.Size(50, 29);
+            this.labelEvery.TabIndex = 3;
+            this.labelEvery.Text = "every:";
+            this.labelEvery.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // checkBox
+            // 
+            this.checkBox.BackColor = System.Drawing.SystemColors.Control;
+            this.checkBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.checkBox.Location = new System.Drawing.Point(0, 0);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(109, 29);
+            this.checkBox.TabIndex = 2;
+            this.checkBox.Text = "Check for email";
+            this.checkBox.UseVisualStyleBackColor = false;
+            // 
+            // FormBlockPollingSetup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelLabel);
+            this.Controls.Add(this.comboBox);
+            this.Name = "FormBlockPollingSetup";
+            this.Size = new System.Drawing.Size(407, 29);
+            this.panelLabel.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Panel panelLabel;
+        private System.Windows.Forms.CheckBox checkBox;
+        private System.Windows.Forms.Label labelEvery;
+
+    }
+}
---
+++ Projects/AowEmailWrapper/Controls/FormBlockPollingSetup.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AowEmailWrapper.Classes;
+using AowEmailWrapper.Localization;
+
+namespace AowEmailWrapper.Controls
+{
+    public partial class FormBlockPollingSetup : BaseFormBlock
+    {
+        private const string PleaseChooseTextKey = "enumChooser";
+        private const string MinutesTextKey = "enumMinutes";
+        private const string DisplayTextTemplate = "{0} {1}";
+        private const int CheckBoxTickWidth = 24;
+
+        public FormBlockPollingSetup()
+        {
+            InitializeComponent();
+            comboBox.DisplayMember = "Text";
+            base.SetControls(panelLabel, comboBox);
+        }
+
+        public string CheckBoxText
+        {
+            get { return checkBox.Text; }
+            set { checkBox.Text = value; }
+        }
+
+        public string EveryText
+        {
+            get { return labelEvery.Text; }
+            set { labelEvery.Text = value; }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            //This must always be on one line
+            Size szEvery = new Size(int.MaxValue, int.MaxValue);
+            szEvery = TextRenderer.MeasureText(labelEvery.Text, labelEvery.Font, szEvery, TextFormatFlags.SingleLine);
+            labelEvery.Size = szEvery;
+
+            int spareWidth = panelLabel.Width - labelEvery.Width;
+
+            Size szCheckBoxText = new Size(spareWidth - CheckBoxTickWidth, int.MaxValue);
+            szCheckBoxText = TextRenderer.MeasureText(checkBox.Text, checkBox.Font, szCheckBoxText, TextFormatFlags.WordBreak);
+
+            checkBox.Width = szCheckBoxText.Width + CheckBoxTickWidth;
+            this.Height = szCheckBoxText.Height + 4;
+        }
+
+        public void AddItem(int value)
+        {
+            AddItem(new ComboBoxItem(value.ToString(), string.Format(DisplayTextTemplate, value, Translator.Translate(MinutesTextKey))));
+        }
+
+        private void AddItem(ComboBoxItem theItem)
+        {
+            if (comboBox.Items.Count.Equals(0))
+            {
+                comboBox.Items.Add(new ComboBoxItem(string.Empty, Translator.Translate(PleaseChooseTextKey)));
+            }
+            comboBox.Items.Add(theItem);
+        }
+
+        public string SelectedValue
+        {
+            get
+            {
+                if (comboBox.SelectedIndex > 0)
+                {
+                    ComboBoxItem theSelectedItem = (ComboBoxItem)comboBox.SelectedItem;
+                    return theSelectedItem.Value;
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                foreach (ComboBoxItem item in comboBox.Items)
+                {
+                    if (item.Value.Equals(value, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        comboBox.SelectedItem = item;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public int SelectedIndex
+        {
+            get { return comboBox.SelectedIndex; }
+            set { comboBox.SelectedIndex = value; }
+        }
+
+        public ComboBox.ObjectCollection Items
+        {
+            get { return comboBox.Items; }
+        }
+
+        public ComboBox InnerComboBox
+        {
+            get { return comboBox; }
+        }
+
+        public bool Checked
+        {
+            get { return checkBox.Checked; }
+            set { checkBox.Checked = value; }
+        }
+
+        public CheckBox InnerCheckBox
+        {
+            get { return checkBox; }
+        }
+    }
+}
