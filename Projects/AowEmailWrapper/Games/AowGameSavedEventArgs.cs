94878408a7a03af27d0c92d2b734ed3a03d38014
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-08-16 07:49:15 +0000

    Modified to accommodate the new info provided by ASGFileInfo / zlib

--- Projects/AowEmailWrapper/Games/AowGameSavedEventArgs.cs
+++ Projects/AowEmailWrapper/Games/AowGameSavedEventArgs.cs
@@ -9,6 +9,9 @@ namespace AowEmailWrapper.Games
     {
         private AowGameType _theGameType;
         private string _fileName;
+        private string _gameTitle;
+        private string _mapTitle;
+        private string _turnNo;
 
         public AowGameType GameType
         {
@@ -22,10 +25,35 @@ namespace AowEmailWrapper.Games
             set { _fileName = value; }
         }
 
+        public string GameTitle
+        {
+            get { return _gameTitle; }
+            set { _gameTitle = value; }
+        }
+
+        public string MapTitle
+        {
+            get { return _mapTitle; }
+            set { _mapTitle = value; }
+        }
+
+        public string TurnNumber
+        {
+            get { return _turnNo; }
+            set { _turnNo = value; }
+        }
+
         public AowGameSavedEventArgs(AowGameType type, string fileName)
+            : this(type, fileName, null, null, null)
+        { }
+
+        public AowGameSavedEventArgs(AowGameType type, string fileName, string gameTitle, string mapTitle, string turnNo)
         {
             _theGameType = type;
             _fileName = fileName;
+            _gameTitle = gameTitle;
+            _mapTitle = mapTitle;
+            _turnNo = turnNo;
         }
     }
 }
