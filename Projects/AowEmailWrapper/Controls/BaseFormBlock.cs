78e0248626ad3d22a38fb96cff2ccb70c5e72e78
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-08-20 17:24:35 +0000

    Renamed a variable that was hiding a base class property

--- Projects/AowEmailWrapper/Controls/BaseFormBlock.cs
+++ Projects/AowEmailWrapper/Controls/BaseFormBlock.cs
@@ -14,7 +14,7 @@ namespace AowEmailWrapper.Controls
         protected Control TheEditControl;
         protected Control TheLabel;
         protected bool resizing = false;
-        private const int Padding = 20;
+        private const int hackPadding = 20;
 
         protected override void OnResize(EventArgs e)
         {
@@ -29,9 +29,9 @@ namespace AowEmailWrapper.Controls
                     int iThirdUnit = (int)Math.Ceiling(dThirdUnit);
                     TheEditControl.Width = (int)(iThirdUnit * 2);
 
-                    TheEditControl.Width -= Padding;
+                    TheEditControl.Width -= hackPadding;
 
-                    Size sz = new Size(iThirdUnit + Padding, int.MaxValue);
+                    Size sz = new Size(iThirdUnit + hackPadding, int.MaxValue);
                     sz = TextRenderer.MeasureText(TheLabel.Text, TheLabel.Font, sz, TextFormatFlags.WordBreak);
                     this.Height = sz.Height;
 
