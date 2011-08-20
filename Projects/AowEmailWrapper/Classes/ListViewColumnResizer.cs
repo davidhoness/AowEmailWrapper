c100d250bec71d440d17e045b0cdbf1acb14c958
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-08-20 17:27:58 +0000

    New class for cosmetic resize of list view columns

---
+++ Projects/AowEmailWrapper/Classes/ListViewColumnResizer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AowEmailWrapper.Helpers;
+
+namespace AowEmailWrapper.Classes
+{
+    public enum ColumnHeaderResizeStyle
+    {
+        None,
+        ColumnContent,
+        HeaderSize,
+        ContentHeaderMax,
+        Fill
+    }
+
+    public static class ListViewColumnResizer
+    {
+        public static void ResizeColumns(ListView theListView)
+        {
+            ColumnHeader fillColumn = null;
+            int totalColumnWidth = 0;
+
+            foreach (ColumnHeader column in theListView.Columns)
+            {
+                if (column.Tag != null)
+                {
+                    ColumnHeaderResizeStyle theStyle = ConfigHelper.ParseEnumString<ColumnHeaderResizeStyle>(column.Tag.ToString());
+
+                    switch (theStyle)
+                    {
+                        case ColumnHeaderResizeStyle.ColumnContent:
+                            column.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+                            totalColumnWidth += column.Width;
+                            break;
+                        case ColumnHeaderResizeStyle.HeaderSize:
+                            column.AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+                            totalColumnWidth += column.Width;
+                            break;
+                        case ColumnHeaderResizeStyle.ContentHeaderMax:
+                            column.AutoResize(ColumnHeaderAutoResizeStyle.HeaderSize);
+                            int headerSize = column.Width;
+                            column.AutoResize(ColumnHeaderAutoResizeStyle.ColumnContent);
+                            int columnContentSize = column.Width;
+
+                            column.Width = (headerSize > columnContentSize) ? headerSize : columnContentSize;
+                            totalColumnWidth += column.Width;
+                            break;
+                        case ColumnHeaderResizeStyle.Fill:
+                            fillColumn = column;
+                            break;
+                        default:
+                            totalColumnWidth += column.Width;
+                            break;
+                    }
+                }
+            }
+
+            if (fillColumn != null)
+            {
+                fillColumn.Width = theListView.Width - (totalColumnWidth + SystemInformation.VerticalScrollBarWidth);
+                fillColumn.Width -= 4;
+            }
+        }
+    }
+}
