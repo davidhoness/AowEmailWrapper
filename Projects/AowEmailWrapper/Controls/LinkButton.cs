3cd6110279af9946226eb72f0eac46e027e2c585
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-08-25 13:55:04 +0000

    New control for launching hyper-links

---
+++ Projects/AowEmailWrapper/Controls/LinkButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using AowEmailWrapper.Helpers;
+
+namespace AowEmailWrapper.Controls
+{
+    public class LinkButton : Button
+    {
+        private string _url;
+
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+
+            if (!string.IsNullOrEmpty(_url))
+            {
+                Process theBrowser = new Process();
+                theBrowser.StartInfo.UseShellExecute = true;
+                theBrowser.StartInfo.FileName = _url;
+                theBrowser.Start();
+            }
+        }
+
+        public LinkButton()
+            : base()
+        {
+            this.Cursor = Cursors.Hand;
+        }
+    }
+}
