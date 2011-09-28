0647ba14372148389af8c97d1a8654fac030c82d
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-09-28 17:05:53 +0000

    New InputBox classed, I knew it would get used elsewhere in the system

---
+++ Projects/AowEmailWrapper/Classes/InputBox.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AowEmailWrapper.Localization;
+
+namespace AowEmailWrapper.Classes
+{
+    public class InputBox
+    {
+        public static DialogResult Show(string title, string promptText, ref string value)
+        {
+            DialogResult dialogResult;
+
+            using (Form form = new Form())
+            {
+                Label label = new Label();
+                TextBox textBox = new TextBox();
+                Button buttonOk = new Button();
+                Button buttonCancel = new Button();
+
+                form.Text = title;
+                label.Text = promptText;
+                textBox.Text = value;
+
+                buttonOk.Text = Translator.Translate("buttonOK");
+                buttonCancel.Text = Translator.Translate("buttonCancel");
+                buttonOk.DialogResult = DialogResult.OK;
+                buttonCancel.DialogResult = DialogResult.Cancel;
+
+                label.SetBounds(9, 20, 372, 13);
+                textBox.SetBounds(12, 36, 372, 20);
+                buttonOk.SetBounds(228, 72, 75, 23);
+                buttonCancel.SetBounds(309, 72, 75, 23);
+
+                label.AutoSize = true;
+                textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+                buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+
+                form.ClientSize = new Size(396, 107);
+                form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+                form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+                form.FormBorderStyle = FormBorderStyle.FixedDialog;
+                form.StartPosition = FormStartPosition.CenterParent;
+                form.MinimizeBox = false;
+                form.MaximizeBox = false;
+                form.AcceptButton = buttonOk;
+                form.CancelButton = buttonCancel;
+                dialogResult = form.ShowDialog();
+
+                if (dialogResult.Equals(DialogResult.OK))
+                {
+                    value = textBox.Text;
+                }
+
+                label.Dispose();
+                textBox.Dispose();
+                buttonOk.Dispose();
+                buttonCancel.Dispose();
+            }
+
+            return dialogResult;
+        }
+    }
+}
