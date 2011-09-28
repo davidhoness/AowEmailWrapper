51ea6fb3363a0a09f95a450c9e953498365a88a3
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-09-28 17:09:21 +0000

    New class for handling email resends

---
+++ Projects/AowEmailWrapper/Helpers/ResendHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using AowEmailWrapper.Games;
+using AowEmailWrapper.Helpers;
+using AowEmailWrapper.Classes;
+using AowEmailWrapper.ConfigFramework;
+using AowEmailWrapper.ASG;
+using Lesnikowski.Mail;
+using Lesnikowski.Mail.Headers;
+
+namespace AowEmailWrapper.Helpers
+{
+    public class ResendHelper
+    {
+        #region Private Members
+        
+        private const string ResendFileNameTemplate = "{0}_resend.eml";
+
+        #endregion
+
+        #region Public Methods
+
+        public static void Save(IMail theEmail)
+        {
+            try
+            {
+                if (theEmail.Attachments.Count > 0)
+                {
+                    theEmail.Save(GetEmlFilePath(theEmail.Attachments[0].FileName));
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+                Trace.Flush();
+            }
+        }
+
+        public static IMail Load(string asgFileName)
+        {
+            IMail returnVal = null;
+
+            try
+            {
+                returnVal = new MailBuilder().CreateFromEmlFile(GetEmlFilePath(asgFileName));
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+                Trace.Flush();
+            }
+
+            return returnVal;
+        }
+
+        public static void Delete(string asgFileName)
+        {
+            try
+            {
+                string filePath = GetEmlFilePath(asgFileName);
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+                Trace.Flush();
+            }
+        }
+
+        public static bool CanResend(string asgFileName)
+        { 
+            string filePath = GetEmlFilePath(asgFileName);
+            return File.Exists(filePath);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static string GetEmlFilePath(string fileName)
+        {
+            return Path.Combine(AppDataHelper.Resend.FullName, string.Format(ResendFileNameTemplate, fileName));
+        }
+
+        #endregion
+    }
+}
