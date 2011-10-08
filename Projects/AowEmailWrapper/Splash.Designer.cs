d8b0c3e61332e06e92363b988067a7de71947297
david.honess <david.honess@141a141f-025e-5042-b5e7-fc004a90e31a>
2011-10-08 13:01:13 +0000

    New splash screen

---
+++ Projects/AowEmailWrapper/Splash.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace AowEmailWrapper
+{
+    partial class Splash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = new System.ComponentModel.Container();
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Splash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::AowEmailWrapper.Properties.Resources.splash;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(366, 273);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Splash";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Splash";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Projects/AowEmailWrapper/Splash.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace AowEmailWrapper
+{
+    public partial class Splash : Form
+    {
+        private static Splash _frmSplash = null;
+        private static Thread _frmThread = null;
+
+        private double _opacityIncrement = .05;
+        private double _opacityDecrement = .1;
+        private const int TIMER_INTERVAL = 50;
+        private System.Windows.Forms.Timer _timer;
+
+        public Splash()
+        {
+            InitializeComponent();
+            this.ClientSize = this.BackgroundImage.Size;
+            this.Opacity = .5;
+
+            _timer = new System.Windows.Forms.Timer(this.components);
+            _timer.Tick += new System.EventHandler(this.timer_Tick);
+            _timer.Interval = TIMER_INTERVAL;
+            _timer.Start();
+        }
+
+        private void timer_Tick(object sender, System.EventArgs e)
+        {
+            if (_opacityIncrement > 0)
+            {
+                if (this.Opacity < 1)
+                    this.Opacity += _opacityIncrement;
+            }
+            else
+            {
+                if (this.Opacity > 0)
+                    this.Opacity += _opacityIncrement;
+                else
+                    this.Close();
+            }
+        }
+
+        private static void ShowForm()
+        {
+            _frmSplash = new Splash();
+            Application.Run(_frmSplash);
+        }
+
+        public static  void ShowSplashScreen()
+        {
+            if (_frmSplash != null)
+                return;
+            _frmThread = new Thread(new ThreadStart(Splash.ShowForm));
+            _frmThread.IsBackground = true;
+            _frmThread.SetApartmentState(ApartmentState.STA);
+            _frmThread.Start();
+        }
+
+        public static void CloseForm()
+        {
+            if (_frmSplash != null && _frmSplash.IsDisposed == false)
+            {
+                // Make it start going away.
+                _frmSplash._opacityIncrement = -_frmSplash._opacityDecrement;
+            }
+            _frmThread = null;	// we don't need these any more.
+            _frmSplash = null;
+        }
+    }
+}
